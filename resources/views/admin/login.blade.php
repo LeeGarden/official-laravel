1e21163b4aa18d60356dccead909a1ea7f9c63a3
Huu Vien Le <mrhuuvien@gmail.com>
2017-06-26 16:40:17 +0700

    Update login.blade.php

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -27,7 +27,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="#"><b>{{ config('app.name') }}</b>Admin</a>
+    <a href="{{  }}"><b>{{ config('app.name') }}</b>Admin</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
